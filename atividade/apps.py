703e483913f672ab1727515cc991b1a53c7b90bb
Iego Assis <35662240-iegoassis@users.noreply.replit.com>
2024-12-15 19:23:29 +0000

    Correção em views

--- atividade/apps.py
+++ atividade/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class AtividadeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'atividade'
-
-INSTALLED_APPS = [
-  'hello',
-  'atividade',
-]
--- django_project/urls.py
+++ django_project/urls.py
@@ -4,7 +4,7 @@ from atividade import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('index/', views.index, name='index'),
-    path('calcular/', views.calcular_view, name='calcular_view'),
-    path('resultado/', views.calcular, name='calcular'),
+    path('calcular/', views.calcular_view, name='calcular_view'),  # Exibe o formulário
+    path('resultado/', views.calcular, name='resultado'),  # Processa os cálculos
     path('autor/', views.autor, name='autor'),
 ]
